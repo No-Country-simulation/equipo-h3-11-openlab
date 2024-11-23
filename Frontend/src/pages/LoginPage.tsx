06ff32f6f3a1a67cdda7eb742cdda9970c69edec
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-11-23 14:19:57 -0300

    feat(responsive): login and register responsive view - add logic localStorage

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -5,14 +5,12 @@ import Footer from '../components/Footer';
 const LoginPage = () => {
   return (
     <>
-      {/* Navbar */}
       <Navbar />
 
       <div className="main-content flex-grow">
         <Login />
       </div>
-      
-      {/* Footer */}
+
       <Footer />
     </>
   )
