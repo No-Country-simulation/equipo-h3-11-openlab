154b12ac1d91f047c221d20f01bd9175751c5d31
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-11-28 17:59:50 -0300

    refactor: Se agregan traducciones a los botones de wallet

--- Frontend/src/components/ConnectWallet.tsx
+++ Frontend/src/components/ConnectWallet.tsx
@@ -1,7 +1,9 @@
 import { useAppKit } from "@reown/appkit/react";
+import { useTranslation } from "react-i18next";
 
 export default function ConnectWallet() {
   const { open } = useAppKit(); // Pasamos appKit manualmente
+  const { t } = useTranslation(["translation"]);
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
@@ -10,13 +12,13 @@ export default function ConnectWallet() {
           onClick={() => open()}
           className="w-full h-12 bg-[#3d7bff] text-white rounded-[10px] text-base font-semibold"
         >
-          Conectar Wallet
+          {t("wallet.connect")}
         </button>
         <button
           onClick={() => open({ view: "Networks" })}
           className="w-full h-12 px-4 py-3 rounded-2xl border border-[#6e6e6e] text-sm flex items-center justify-center space-x-2"
         >
-          Seleccionar Red
+          {t("wallet.network")}
         </button>
       </div>
     </div>
