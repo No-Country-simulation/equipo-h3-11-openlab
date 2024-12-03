da36b67d9c7dbfbdcd87196fbbd5f6f0d86bc8fe
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-12-02 23:12:31 -0300

    feat: se incluye encabezado de iniciativas

--- Frontend/src/pages/Initiatives.tsx
+++ Frontend/src/pages/Initiatives.tsx
@@ -7,9 +7,15 @@ const Initiatives = () => {
     return (
         <>
             <Header />
-            <div className="main-content flex-grow relative">
-                <Sidebar />
-                <InitiativesHeader />
+            <div className="main-content flex-grow relative bg-slate-50">
+                <div className="flex flex-row space-x-8">
+                    <div className="basis-1/6">
+                        <Sidebar />
+                    </div>
+                    <div className="basis-5/6">
+                        <InitiativesHeader />
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
