489b33e1576e0ba147b9dd66b0a084aa9ec5150b
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-12-05 20:20:49 -0300

    feat: Se agrega DataTable

--- Frontend/src/components/Initiatives/DataTable.tsx
+++ Frontend/src/components/Initiatives/DataTable.tsx
@@ -1,39 +1,81 @@
+import { useState } from "react"
 import { Heart, Share2 } from "lucide-react"
+import { useTranslation } from "react-i18next"
+import { flexRender, getCoreRowModel, useReactTable, getPaginationRowModel } from "@tanstack/react-table"
+import dataExample from "../../data/MOCK_DATA.json"
+
 
 const DataTable = () => {
+    const { t } = useTranslation(["translation"]);
+    const [data, setData] = useState(dataExample)
+
+    const columns = [
+        {
+            accessorKey: "company",
+        },
+        {
+            accessorKey: "employees",
+        },
+        {
+            accessorKey: "value",
+        },
+        {
+            accessorKey: "actions",
+        },
+        {
+            accessorKey: "projects",
+        },
+        {
+            accessorKey: "likes",
+        },
+        {
+            accessorKey: "shares",
+        }
+    ]
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel()
+    })
+
     return (
+        <>
         <table className="table-auto w-full bg-white border-collapse">
-            <thead className="bg-blue-200">
-                <tr>
-                    <th>Name</th>
-                    <th>Price Fluctuation</th>
-                    <th>Collaborators</th>
-                    <th>Buy/Sell Price</th>
-                    <th>Tokens</th>
-                    <th>Missions</th>
-                    <th>Likes</th>
-                    <th>Shares</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
+            <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id} className="border border-transparent bg-blue-200">
+                        {headerGroup.headers.map(header => (
+                            <th key={header.id}>
+                                {header.isPlaceholder ? null : flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext()
+                                )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
             </thead>
             <tbody>
-            <tr>
-                <td>Fractal</td>
-                <td>--</td>
-                <td>140</td>
-                <td>150/140</td>
-                <td>120k/450</td>
-                <td>34/346</td>
-                <td>100k</td>
-                <td>220</td>
-                <td></td>
-                <td><button>Join</button></td>
-                <td><Heart /><Share2 /></td>
-            </tr>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id} className="border-b border-gray-300">
+                        {row.getVisibleCells().map(cell => (
+                            <td key={cell.id}>
+                                {flexRender(
+                                    cell.column.columnDef.cell,
+                                    cell.getContext()
+                                )}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
             </tbody>
         </table>
+        <div className="mt-4 flex items-center justify-between">
+
+        </div>
+        </>
     )
 }
 
