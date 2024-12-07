7dd93c693679a30edcf732163c1124ff91860c89
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-12-07 18:08:05 -0300

    fix: Se realizan ajustes en la integración de ramas staging y devFrontend

--- Frontend/src/pages/Settings.tsx
+++ Frontend/src/pages/Settings.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
+import { MonitorCog } from 'lucide-react'
 
 const Settings = () => {
     return (
@@ -9,7 +10,10 @@ const Settings = () => {
             <div className="main-content flex-grow relative">
                 <Sidebar />
                 <section className='flex flex-col items-center'>
-                    <h1>Este es la pagina de ajustes</h1>
+                    <div className="flex flex-col justify-center items-center w-2/4 h-full bg-blue-300 font-bold text-2xl rounded-xl shadow mx-8 my-16 py-16">
+                    <MonitorCog className='w-24 h-24 m-8' />
+                    <span>Esta página se encuentra en construcción, disculpe las molestias.</span>
+                    </div>
                 </section>
             </div>
             <Footer />
