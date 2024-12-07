6928570f49bd772ade21e2ddb75ddd64b6403c16
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-12-07 16:35:38 -0300

    fix: Se arreglan detalles de paginación y columna de acciones

--- Frontend/src/components/Initiatives/DataTable.tsx
+++ Frontend/src/components/Initiatives/DataTable.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
-import { Heart, Share2 } from "lucide-react"
+import { Rewind, FastForward, StepBack, StepForward, Heart, Share2 } from "lucide-react"
 import { useTranslation } from "react-i18next"
 import { FilterFn, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, getFilteredRowModel } from "@tanstack/react-table"
-import { RankingInfo, rankItem, compareItems } from "@tanstack/match-sorter-utils"
+import { RankingInfo, rankItem } from "@tanstack/match-sorter-utils"
 import classNames from "classnames"
 import SearchBar from "../SearchBar"
 import InitiativesFilters from "./Filters"
 import dataExample from "../../data/ExampleData.json"
+import fluctuation from "../../assets/price-fluctuation.png";
 
 declare module '@tanstack/react-table' {
     //add fuzzy filter to the filterFns
@@ -30,6 +31,8 @@ const DataTable = () => {
     const { t } = useTranslation(["translation"]);
     const [data, setData] = useState(dataExample)
     const [searchFilter, setSearchFilter] = useState("")
+    console.log(searchFilter);
+    
 
     const columns = [
         {
@@ -38,7 +41,14 @@ const DataTable = () => {
         },
         {
             accessorKey: "priceFluctation",
-            header: () => <span>{t("initiativesOptions.priceFluctuation")}</span>
+            header: () => <span>{t("initiativesOptions.priceFluctuation")}</span>,
+            cell: () => {
+                return (
+                    <div>
+                        <img src={fluctuation} />
+                    </div>
+                )
+            }
         },
         {
             accessorKey: "collaborators",
@@ -65,8 +75,34 @@ const DataTable = () => {
         {
             accessorKey: "shares",
             header: () => <span>{t("initiativesOptions.shares")}</span>
+        },
+        {
+            accessorKey: "actions",
+            header: () => <span>{t("initiativesOptions.actions")}</span>,
+            cell: (info: any) => {
+                return (
+                    <div className="flex flex-row no-wrap space-x-2">
+                        <button className="bg-blue-500 rounded-xl text-white px-1 py-2 font-semibold">{t("initiativesOptions.buy")}</button>
+                        <button className="bg-indigo-700 rounded-xl text-white px-1 py-2 font-semibold">{t("initiativesOptions.join")}</button>
+                        <button><Heart /></button>
+                        <button><Share2 /></button>
+                    </div>
+                )
+            }
         }
     ]
+
+    const getStateTable = () => {
+        const totalRows = table.getFilteredRowModel().rows.length
+        const pageSize = table.getState().pagination.pageSize
+        const pageIndex = table.getState().pagination.pageIndex
+        const rowsPerPage = table.getRowModel().rows.length
+
+        const firstIndex = (pageSize * pageIndex) + 1
+        const lastIndex = (pageSize * pageIndex) + rowsPerPage
+
+        return {totalRows, firstIndex, lastIndex}
+    }
 
     const table = useReactTable({
         data,
@@ -87,7 +123,7 @@ const DataTable = () => {
             value={searchFilter ?? ""}
         />
         <InitiativesFilters />
-        <table className="table-fixed w-full bg-white border-collapse">
+        <table className="table-auto w-full bg-white border-collapse">
             <thead>
                 {table.getHeaderGroups().map(headerGroup => (
                     <tr key={headerGroup.id} className="border border-transparent bg-blue-200">
@@ -124,14 +160,14 @@ const DataTable = () => {
                     disabled:bg-white disabled:text-gray-300"
                     onClick={() => table.setPageIndex(0)}
                     disabled={!table.getCanPreviousPage()}>
-                    {"<<"}
+                    {<Rewind />}
                 </button>
                 <button
                     className="text-gray-600 bg-gray-200 py-0.5 px-1 border rounded border-gray-300
                     disabled:bg-white disabled:text-gray-300"
                     onClick={() => table.previousPage()}
                     disabled={!table.getCanPreviousPage()}>
-                    {"<"}
+                    {<StepBack />}
                 </button>
                 
                 {table.getPageOptions().map((value, key) => (
@@ -150,29 +186,30 @@ const DataTable = () => {
                     disabled:bg-white disabled:text-gray-300"
                     onClick={() => table.nextPage()}
                     disabled={!table.getCanNextPage()}>
-                    {">"}
+                    {<StepForward />}
                 </button>
                 <button
                     className="text-gray-600 bg-gray-200 py-0.5 px-1 border rounded border-gray-300
                     disabled:bg-white disabled:text-gray-300"
                     onClick={() => table.setPageIndex(table.getPageCount() - 1)}
                     disabled={!table.getCanNextPage()}>
-                    {">>"}
+                    {<FastForward />}
                 </button>
             </div>
             <div className="text-gray-600 font-semibold">
-                Mostrando de {Number(table.getRowModel().rows[0]?.id) + 1}&nbsp;
-                a {Number(table.getRowModel().rows[table.getRowModel().rows.length - 1]?.id) + 1}&nbsp;
-                del total de {dataExample.length} registros 
+                {t("initiativesOptions.showing")}  {getStateTable().firstIndex}&nbsp;
+                {t("initiativesOptions.to")}  {getStateTable().lastIndex}&nbsp;
+                {t("initiativesOptions.ofTotal")} {getStateTable().totalRows}&nbsp;
+                {t("initiativesOptions.rows")} 
             </div>
             <select
                 className="text-gray-600 border border-gray-300 rounded outline-blue-500"
                 onChange={e => {
                     table.setPageSize(Number(e.target.value))
                 }}>
-                <option value="10">10 filas</option>
-                <option value="25">25 filas</option>
-                <option value="50">50 filas</option>
+                <option value="10">10 {t("initiativesOptions.rows")}</option>
+                <option value="25">25 {t("initiativesOptions.rows")}</option>
+                <option value="50">50 {t("initiativesOptions.rows")}</option>
             </select>
         </div>
         </>
